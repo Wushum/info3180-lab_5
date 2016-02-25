13f86cb5403f21ea5b13c475eb6130788e1ad64e
Winston Lindsay <wnlindsay@yahoo.com>
2016-02-25 02:25:25 +0000

    first commit

--- app/models.py
+++ app/models.py
@@ -5,6 +5,11 @@ class Myprofile(db.Model):
     last_name = db.Column(db.String(80)) 
     nickname = db.Column(db.String(80), unique=True)    
     email = db.Column(db.String(120), index=True, unique=True)
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
  
 
     
@@ -25,3 +30,25 @@ class Myprofile(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+        
+    @app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(user)
+
+        flask.flash('Logged in successfully.')
+
+        next = flask.request.args.get('next')
+        # next_is_valid should check if the user has valid
+        # permission to access the `next` url
+        if not next_is_valid(next):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('index'))
+    return flask.render_template('login.html', form=form)
